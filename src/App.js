cf0a5cfd01c250ac8de07bd3f03d4981051ba890
bill <you@example.com>
2020-12-29 16:08:32 +0900

    Todos 프리젠이션널 컴포넌트, 컨테이너 컴포넌트 개발

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
- 
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
-  return <CounterContainer></CounterContainer>;
+  return (
+    <>
+      <CounterContainer></CounterContainer>
+      <hr/>
+      <TodosContainer></TodosContainer>
+    </>
+  );
 }
 
 export default App;
